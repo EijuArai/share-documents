b923bd6dbd4d4c71fff37c3416a72f67f615cb02
E.Arai <eiju@EijuMac.local>
2023-09-12 21:34:40 +0900

    Fixed bugs

--- contracts/src/main/kotlin/com/template/contracts/DocumentContract.kt
+++ contracts/src/main/kotlin/com/template/contracts/DocumentContract.kt
@@ -27,98 +27,99 @@ class DocumentContract : Contract {
                 "There should be no DocumentState in the input".using(tx.inRefsOfType<DocumentState>().isEmpty())
                 "There should be exactly one DocumentState in the output".using(tx.outputsOfType<DocumentState>().size == 1)
 
-                val output = tx.outputsOfType<DocumentState>().first()
+                val output = tx.outputsOfType<DocumentState>().single()
 
                 "Proposer and Consenter must be different parties".using(output.proposer != output.consenter)
+                "Participants should be Proposer and Consenter".using(output.participants == listOf(output.proposer, output.consenter))
                 "The initial status of the output should be PROPOSED".using(output.status == DocumentStatus.PROPOSED)
-                "The version should be 0".using(output.versionNo == 0)
+                "The versionNo should be 0".using(output.versionNo == 0)
 
-                val signers = tx.commands[0].signers.toSet()
-                val participants = tx.outputStates[0].participants.map(AbstractParty::owningKey).toSet()
+                val signers = tx.commands.single().signers.distinct()
+                val distinctParticipants = tx.outputStates.single().participants.map(AbstractParty::owningKey).distinct()
 
-                "Only required signers may sign the transaction".using(signers == participants)
+                "Only required signers may sign the transaction".using(signers == distinctParticipants)
             }
 
             is Commands.Reject -> requireThat {
                 "There should be exactly one DocumentState in the input".using(tx.inRefsOfType<DocumentState>().size == 1)
                 "There should be exactly one DocumentState in the output".using(tx.outputsOfType<DocumentState>().size == 1)
 
-                val input = tx.inputsOfType<DocumentState>().first()
-                val output = tx.outputsOfType<DocumentState>().first()
+                val input = tx.inputsOfType<DocumentState>().single()
+                val output = tx.outputsOfType<DocumentState>().single()
 
                 "Proposer and Consenter must be different parties".using(output.proposer != output.consenter)
+                "Participants should be Proposer and Consenter".using(output.participants == listOf(output.proposer, output.consenter))
                 "The status of the input should be PROPOSED".using(input.status == DocumentStatus.PROPOSED)
                 "The status of the output should be REJECTED".using(output.status == DocumentStatus.REJECTED)
-                "The comments field should not be null or empty".using(!output.comments.isNullOrEmpty())
-                "The version should be greater than 0".using(output.versionNo > 0)
-                "The version should be incremented".using(output.versionNo == input.versionNo + 1)
-                "Properties other than version, status, comments and updateTime should not have been changed".using(
+                "The comments should not be null or empty".using(!output.comments.isNullOrEmpty())
+                "The versionNo should be greater than 0".using(output.versionNo > 0)
+                "The versionNo should be incremented".using(output.versionNo == input.versionNo + 1)
+                "Properties other than versionNo, status, comments and updateTime should not have been changed".using(
                     output.proposer == input.proposer &&
                             output.consenter == input.consenter &&
                             output.documentTitle == input.documentTitle &&
                             output.documentType == input.documentType &&
                             output.documentJson == input.documentJson &&
-                            output.linearId == input.linearId
+                            output.linearId == input.linearId &&
+                            output.participants == input.participants
                 )
-                val signers = tx.commands[0].signers.toSet()
-                val participantsInOutput = tx.outputStates[0].participants.map(AbstractParty::owningKey).toSet()
-                val participantsInInput = tx.inputStates[0].participants.map(AbstractParty::owningKey).toSet()
-                val participants = participantsInOutput + participantsInInput
+                val signers = tx.commands.single().signers.distinct()
+                val distinctParticipants = (input.participants + output.participants).map(AbstractParty::owningKey).distinct()
 
-                "Only required signers may sign the transaction".using(signers == participants)
+                "Only required signers may sign the transaction".using(signers == distinctParticipants)
             }
 
             is Commands.Revise -> requireThat {
                 "There should be exactly one DocumentState in the input".using(tx.inRefsOfType<DocumentState>().size == 1)
                 "There should be exactly one DocumentState in the output".using(tx.outputsOfType<DocumentState>().size == 1)
 
-                val input = tx.inputsOfType<DocumentState>().first()
-                val output = tx.outputsOfType<DocumentState>().first()
+                val input = tx.inputsOfType<DocumentState>().single()
+                val output = tx.outputsOfType<DocumentState>().single()
 
                 "Proposer and Consenter must be different parties".using(output.proposer != output.consenter)
+                "Participants should be Proposer and Consenter".using(output.participants == listOf(output.proposer, output.consenter))
                 "The status of the input should be PROPOSED or REJECTED".using(input.status == DocumentStatus.PROPOSED || input.status == DocumentStatus.REJECTED)
                 "The status of the output should be PROPOSED".using(output.status == DocumentStatus.PROPOSED)
-                "The version should be greater than 0".using(output.versionNo > 0)
-                "The version should be incremented".using(output.versionNo == input.versionNo + 1)
-                "Properties other than status, documentTitle, documentType, documentJson, comments and updateTime should not have been changed".using(
+                "The versionNo should be greater than 0".using(output.versionNo > 0)
+                "The versionNo should be incremented".using(output.versionNo == input.versionNo + 1)
+                "Properties other than status, documentTitle, documentType, documentJson, comments, versionNo and updateTime should not have been changed".using(
                     output.proposer == input.proposer &&
                             output.consenter == input.consenter &&
-                            output.linearId == input.linearId
+                            output.linearId == input.linearId &&
+                            output.participants == input.participants
                 )
-                val signers = tx.commands[0].signers.toSet()
-                val participantsInOutput = tx.outputStates[0].participants.map(AbstractParty::owningKey).toSet()
-                val participantsInInput = tx.inputStates[0].participants.map(AbstractParty::owningKey).toSet()
-                val participants = participantsInOutput + participantsInInput
+                val signers = tx.commands.single().signers.distinct()
+                val distinctParticipants = (input.participants + output.participants).map(AbstractParty::owningKey).distinct()
 
-                "Only required signers may sign the transaction".using(signers == participants)
+                "Only required signers may sign the transaction".using(signers == distinctParticipants)
             }
 
             is Commands.Approve -> requireThat {
                 "There should be exactly one DocumentState in the input".using(tx.inRefsOfType<DocumentState>().size == 1)
                 "There should be exactly one DocumentState in the output".using(tx.outputsOfType<DocumentState>().size == 1)
 
-                val input = tx.inputsOfType<DocumentState>().first()
-                val output = tx.outputsOfType<DocumentState>().first()
+                val input = tx.inputsOfType<DocumentState>().single()
+                val output = tx.outputsOfType<DocumentState>().single()
 
                 "Proposer and Consenter must be different parties".using(output.proposer != output.consenter)
+                "Participants should be Proposer and Consenter".using(output.participants == listOf(output.proposer, output.consenter))
                 "The status of the input should be PROPOSED".using(input.status == DocumentStatus.PROPOSED)
                 "The status of the output should be APPROVED".using(output.status == DocumentStatus.APPROVED)
-                "The version should be greater than 0".using(output.versionNo > 0)
-                "The version should be incremented".using(output.versionNo == input.versionNo + 1)
-                "Properties other than status, version, comments and updateTime should not have been changed".using(
+                "The versionNo should be greater than 0".using(output.versionNo > 0)
+                "The versionNo should be incremented".using(output.versionNo == input.versionNo + 1)
+                "Properties other than status, versionNo, comments and updateTime should not have been changed".using(
                     output.proposer == input.proposer &&
                             output.consenter == input.consenter &&
                             output.documentTitle == input.documentTitle &&
                             output.documentType == input.documentType &&
                             output.documentJson == input.documentJson &&
-                            output.linearId == input.linearId
+                            output.linearId == input.linearId &&
+                            output.participants == input.participants
                 )
-                val signers = tx.commands[0].signers.toSet()
-                val participantsInOutput = tx.outputStates[0].participants.map(AbstractParty::owningKey).toSet()
-                val participantsInInput = tx.inputStates[0].participants.map(AbstractParty::owningKey).toSet()
-                val participants = participantsInOutput + participantsInInput
+                val signers = tx.commands.single().signers.distinct()
+                val distinctParticipants = (input.participants + output.participants).map(AbstractParty::owningKey).distinct()
 
-                "Only required signers may sign the transaction".using(signers == participants)
+                "Only required signers may sign the transaction".using(signers == distinctParticipants)
             }
         }
     }
